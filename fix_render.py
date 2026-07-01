752386d05f49b0ce28294e4590119fe3979b291c
Orledis Oliveros <tu@email.comcd>
2026-07-01 18:59:09 -0400

    chore: remove temporary fix scripts

--- fix_render.py
+++
@@ -1,7 +0,0 @@
-with open('app.js', 'r') as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    line_num = i + 1
-    if 260 <= line_num <= 285:
-        print(f"{line_num}: {line}", end='')
